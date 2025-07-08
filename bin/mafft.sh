909665d9ff8bf7d9b949e7d62bf324f68684df67
Dhinesh Periyasamy <64551109+dhineshp565@users.noreply.github.com>
2025-07-08 14:15:17 -0600

    added scripts for msa and ggtree

---
+++ bin/mafft.sh
@@ -0,0 +1,24 @@
+#!/bin/env bash
+# This script runs MAFFT on a given input file and outputs the result to a specified output file.
+# list of viruses to process
+# $1 = path to the directory containing reference sequences
+virus_list=("FAdV" "IBDV" "IBV")
+
+# concatenate all the consensus sequences into one file
+cat *.fasta > all_consensus.fasta
+
+# loop through each virus in the list
+for virus in "${virus_list[@]}"; do
+    # check if the virus is present in the concatenated file
+    if grep -q "$virus" all_consensus.fasta; then
+		# extract sequences for the virus and create a new file
+		awk -v v="$virus" '/^>/ {keep = ($0 ~ v)} keep' all_consensus.fasta > "${virus}_only.fasta"
+		# concatenate the extracted sequences with the reference sequences
+        cat "${virus}_only.fasta" $1/${virus}_references.fasta > ${virus}_unaligned.fasta
+		# run mafft on the concatenated file
+        mafft --auto ${virus}_unaligned.fasta > ${virus}_msa.fasta
+	# if the virus is not present, create an empty file
+	else
+		echo ">Novirusfromlist" > no_msa.fasta
+    fi
+done
---
+++ bin/plot_tree.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env Rscript
+
+args <- commandArgs(trailingOnly=TRUE)
+treefile <- args[1]
+
+library(ggtree)
+library(ape)
+library(dplyr)
+library(ggplot2)
+
+tree <- read.tree(treefile)
+
+tip_df <- data.frame(label = tree$tip.label) %>%
+    mutate(color = ifelse(grepl("PDS", label), "red", "black"))
+
+p <- ggtree(tree, layout='rectangular', size=0.2) %<+% tip_df +
+    geom_tiplab(aes(color = color), size = 2.0) +
+    scale_color_identity() +
+    xlim(0,9) +
+    geom_text2(aes(subset = !isTip, label = label), hjust = -0.2, size =1.5) +
+    geom_treescale(x = 0, y = 10, width = 0.05, fontsize = 1.5, linesize = 0.5) +
+    theme_tree2() +
+    labs(caption="nucleotide substitutions per site")
+
+out_file <- paste0("tree_", tools::file_path_sans_ext(basename(treefile)), ".png")
+ggsave(out_file, p, width = 15, height = 10, units = "cm", dpi = 300)
